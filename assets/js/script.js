935f601f90012932296a13c0cd7aab7dd6e219ea
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-05-17 17:32:27 +0200

    girds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 $('.developerShow').toggle(); 
 $('.videoGridShow').toggle(); 
-$('h1').css('text-shadow','1px 1px 3px #d5f131')
-$('h1').css('color','#2b3ddd;')
+$('h1').css('text-shadow','1px 1px 3px #d5f131');
+$('h1').css('color','#2b3ddd;');
+let motion = false;
+
+while(motion===true){
+$('.videoGridShow h3').css('text-shadow','10px 10px 100px rgb(223, 236, 150)');
+$('.videoGridShow h3').css('text-shadow','1px 1px 1px rgb(223, 236, 150)');
+
+}
 
 $('.web').on('click', ()=>{
     $('.developerHide').toggle(); 
@@ -11,8 +18,11 @@ $('.web').on('click', ()=>{
 })
 
 $('.motion').on('click', ()=>{
+    motion = true; 
+    console.log(motion);    
     $('.videoGridHide').toggle(); 
     $('.motion').css('text-shadow','1px 1px 3px #dfec96')
     $('.motion').css('width','70%')
     $('.motion').css('border-bottom','#06159e 1px solid;')
 })
+
